60593d16b6b67fc18fdae6889a1a6422e45f7438
mahin8514 <mahin@artificerstechnologies.com>
2016-12-05 11:45:49 +0530

    Test

---
+++ ScrollViewAutolayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewAutolayout
+//
+//  Created by Mahin Ibrahim on 12/2/16.
+//  Copyright © 2016 Mahin Ibrahim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewAutolayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewAutolayout
+//
+//  Created by Mahin Ibrahim on 12/2/16.
+//  Copyright © 2016 Mahin Ibrahim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
